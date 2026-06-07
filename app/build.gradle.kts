7f1e20226d4beb53cd279e1e7fea57e71a97573d
noamtu123 <noamtu123@gmail.com>
2026-06-08 02:21:49 +0300

    Prepare alpha 0.3.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.turel.jewishdaynext"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "0.3.0"
+        versionCode = 5
+        versionName = "0.3.1"
     }
 
     buildTypes {
