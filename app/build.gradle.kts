52c333b7a04b21a6d0790c6f2a132b23585df801
noamtu123 <noamtu123@gmail.com>
2026-06-18 18:00:13 +0300

    Bump to 0.4.3 for develop test build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.noamtu.jewishday"
         minSdk = 26
         targetSdk = 36
-        versionCode = 8
-        versionName = "0.4.2"
+        versionCode = 9
+        versionName = "0.4.3"
     }
 
     signingConfigs {
