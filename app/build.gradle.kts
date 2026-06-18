f6bff6592d2ad2776b93993869515f84707512e5
noamtu123 <noamtu123@gmail.com>
2026-06-18 17:27:03 +0300

    Prepare alpha 0.4.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.noamtu.jewishday"
         minSdk = 26
         targetSdk = 36
-        versionCode = 7
-        versionName = "0.4.1"
+        versionCode = 8
+        versionName = "0.4.2"
     }
 
     signingConfigs {
