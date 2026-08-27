89cce6cc9ef33e96df807407c30e92ebb51c910b
noamtu123 <noamtu123@gmail.com>
2026-08-28 00:21:58 +0300

    Prepare 1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.noamtu.jewishday"
         minSdk = 26
         targetSdk = 36
-        versionCode = 15
-        versionName = "0.9.0"
+        versionCode = 16
+        versionName = "1.0.0"
     }
 
     signingConfigs {
